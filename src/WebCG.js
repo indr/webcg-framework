8c5c3ec7d4e9bcb41e41f89d958ff78f3cecf277
indr <mail@indr.ch>
2018-09-20 17:07:49 +0800

    feat(*): add listeners for custom invokable functions

--- src/WebCG.js
+++ src/WebCG.js
@@ -1,13 +1,16 @@
 import Parser from './Parser.js'
 
+const FUNCTIONS = ['play', 'stop', 'next', 'update']
+
 class WebCG {
   constructor (window) {
     this._listeners = {}
+    this._window = window
 
-    window.play = this.play.bind(this)
-    window.stop = this.stop.bind(this)
-    window.next = this.next.bind(this)
-    window.update = this.update.bind(this)
+    FUNCTIONS.forEach(each => {
+      this._window[each] = this[each].bind(this)
+      this._window[each].webcg = true
+    })
 
     // Aliases
     this.on = this.addEventListener
@@ -18,6 +21,7 @@ class WebCG {
     if (typeof listener !== 'function') throw new TypeError('listener must be a function')
     const listeners = this._listeners[type] = this._listeners[type] || []
     listeners.push(listener)
+    this._addWindowFunction(type)
   }
 
   removeEventListener (type, listener) {
@@ -26,25 +30,42 @@ class WebCG {
     if (idx >= 0) {
       listeners.splice(idx, 1)
     }
+
+    if (listeners.length === 0) {
+      this._removeWindowFunction(type)
+    }
+  }
+
+  _addWindowFunction (name) {
+    if (typeof this._window[name] === 'function' && this._window[name].webcg) return
+
+    this._window[name] = this.dispatch.bind(this, name)
+    this._window[name].webcg = true
+  }
+
+  _removeWindowFunction (name) {
+    if (FUNCTIONS.indexOf(name) >= 0) return
+    if (typeof this._window[name] !== 'function' || !this._window[name].webcg) return
+    delete this._window[name]
   }
 
   play () {
-    this._dispatch('play')
+    this.dispatch('play')
   }
 
   stop () {
-    this._dispatch('stop')
+    this.dispatch('stop')
   }
 
   next () {
-    this._dispatch('next')
+    this.dispatch('next')
   }
 
   update (data) {
-    const handled = this._dispatch('update', data)
+    const handled = this.dispatch('update', data)
     if (!handled) {
       const parsed = new Parser().parse(data)
-      this._dispatch('data', parsed)
+      this.dispatch('data', parsed)
     }
   }
 
@@ -53,7 +74,7 @@ class WebCG {
     return this._listeners[type]
   }
 
-  _dispatch (type, arg) {
+  dispatch (type, arg) {
     const listeners = this._getListeners(type)
     let handled = false
     for (let i = listeners.length - 1; i >= 0 && handled === false; i--) {
